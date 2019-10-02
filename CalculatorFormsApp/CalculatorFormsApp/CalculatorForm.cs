0631cdc79bc78a6433655f3c6a5b23415b4b47f8
lHasib <hasibuzzaman25@gmail.com>
2019-10-03 02:49:32 +0600

    CaculatorAppAdded

---
+++ CalculatorFormsApp/CalculatorFormsApp/CalculatorForm.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculatorFormsApp
+{
+    public partial class CalculatorForm : Form
+    {
+        string firstnumber;
+        string secondnumber;
+        string result;
+
+        public CalculatorForm()
+        {
+            InitializeComponent();
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            firstnumber = firstNumberTextBox.Text;
+            secondnumber = secondNumberTextBox.Text;
+            double f = Convert.ToInt32(firstnumber);
+            double s = Convert.ToInt32(secondnumber);
+            double r = s + f;
+            result = r.ToString();
+            resultTextBox.Text = result;
+
+
+        }
+
+        private void subtractButton_Click(object sender, EventArgs e)
+        {
+            firstnumber = firstNumberTextBox.Text;
+            secondnumber = secondNumberTextBox.Text;
+            double f = Convert.ToInt32(firstnumber);
+            double s = Convert.ToInt32(secondnumber);
+            double r = f - s;
+            result = r.ToString();
+            resultTextBox.Text = result;
+        }
+
+        private void multiplyButton_Click(object sender, EventArgs e)
+        {
+            firstnumber = firstNumberTextBox.Text;
+            secondnumber = secondNumberTextBox.Text;
+            double f = Convert.ToInt32(firstnumber);
+            double s = Convert.ToInt32(secondnumber);
+            double r = f * s;
+            result = r.ToString();
+            resultTextBox.Text = result;
+        }
+
+        private void divideButton_Click(object sender, EventArgs e)
+        {
+            firstnumber = firstNumberTextBox.Text;
+            secondnumber = secondNumberTextBox.Text;
+            double f = Convert.ToInt32(firstnumber);
+            double s = Convert.ToInt32(secondnumber);
+            double r = f / s;
+            result = r.ToString();
+            resultTextBox.Text = result;
+            
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            firstNumberTextBox.Clear();
+            secondNumberTextBox.Clear();
+            resultTextBox.Clear();
+        }
+    }
+}
